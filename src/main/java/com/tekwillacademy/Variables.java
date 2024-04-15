654bcecdf39d4499bd1daa99fbfbc509f600c8ae
andreice <aleefve@gmail.com>
2024-04-15 17:40:00 +0300

    Added the Variables and Comments Task

---
+++ src/main/java/com/tekwillacademy/Variables.java
@@ -0,0 +1,39 @@
+package com.tekwillacademy;
+
+public class Variables {
+    public static void main(String[] args) {
+
+    // Declarare
+        byte byteVar;
+        short shortVar;
+        int intVar;
+        long longVar;
+        float floatVar;
+        double doubleVar;
+        char charVar;
+        boolean booleanVar;
+        String stringVar;
+
+        // Inițializare
+        byteVar = 100;
+        shortVar = 1000;
+        intVar = 10000;
+        longVar = 100000L;
+        floatVar = 3.14f;
+        doubleVar = 3.14159;
+        charVar = 'A';
+        booleanVar = true;
+        stringVar = "Hello, World!";
+
+        // Afișare
+        System.out.println("byteVar = " + byteVar);
+        System.out.println("shortVar = " + shortVar);
+        System.out.println("intVar = " + intVar);
+        System.out.println("longVar = " + longVar);
+        System.out.println("floatVar = " + floatVar);
+        System.out.println("doubleVar = " + doubleVar);
+        System.out.println("charVar = " + charVar);
+        System.out.println("booleanVar = " + booleanVar);
+        System.out.println("stringVar = " + stringVar);
+    }
+}
